5af09fb51e5b7e3b029650198c754a05f43ccaf2
Frank1626 <arturofco26@gmail.com>
2025-03-11 16:09:17 -0600

    Fuente y boton personalizados añadidos

---
+++ Assets/VuforiaLicense.cs
@@ -0,0 +1,14 @@
+
+            //This class is auto-generated, please do not overwrite
+            using UnityEngine.Scripting;
+
+            public class VuforiaLicense
+            {
+const string license = "QVRPdzhpZi8vLy8vQUFBQm1kc3gwU1dFSVVOVG0zMk9XcW0wVDNRbUxISnhUY09ybElnMzI4WXorTjBvSmRZRDFraEhvQS84RS9haTV0UEFEZ3NQdWU4TExHeWdqbGtkWjFXam9VTzhhN3UrWlFIeXoxNmV2YkhyRHE2d0Rqb2k1OE9sVkFHcm1ZNEZqWXN6NVJNcHdvN0JQRGFBMGcwY0ZRdmh5RFErWFRyeXd0Y0FmaVg3NksxVGJOQzlMbGtGTFU1d3RDaDFlN2ZtaVVtWVp4RXNBWDN1ckg2UG5IOW1uSUdNM3dxWjJYQlVnWnhkS2ppMkhPMU1UYmlCYmcrWEt2bnBNWHhjSHFlS29hYm5KWDE2V3hqd0kxR1lMQmNmMm53VEgzQWZBbGtKaTc0c1JLNU94dlNibStLUE9kSTFERGpUVGhNTDRTRC9qUXpGbmlQREJWeTZGblp5L1JRNSs4UlA5bXZXSy8yUmNLK01KVmFURTBiTUkwdXM=";
+
+                [Preserve]
+                static string GetLicenseKey()
+                {
+                    return license;
+                }
+            }
